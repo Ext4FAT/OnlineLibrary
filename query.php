287a1b949ccbf4d1e1f2b9a7cf057eb292e71896
zyy34472 <zyy34472@gmail.com>
2016-08-07 03:09:03 +0800

    complete [push to kindle]

--- query.php
+++ query.php
@@ -2,39 +2,56 @@
     <head>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta http-equiv="Content-Style-Type" content="text/css" />
-	  <title> Technique Books Library </title>
+	  <title> Technique E-Books Library </title>
       <style type="text/css">code{white-space: pre;}</style>
       <link rel="stylesheet" href="./css/github2.css" type="text/css" />
     </head>
 	<body>
 		<h1> Technique Books Library </h1>
         
-        <form action="testphp.php" method="post">
+        <!-- Post Query--> 
+        <form action="query.php" method="post">
              <input type="text"
                     value="<?php if(isset($_POST['keyword'])) {echo $_POST['keyword'];} ?>" 
                     name="keyword" >
-             
-             <!--<input type="text" name="e-mail">-->
 			 <input type="submit" value="Search">
         </form>
         
+        <!-- Process Query -->
         <form action="process.php" method="post">
 		<?php
-			header("Content-type: text/html; charset=utf-8");
+			function getFileType($filename) {
+				$extern = explode('.', $filename);
+				return end($extern);
+			}
+			
+			function getBasename($path) {
+				$extern = explode('/', $path);
+				return end($extern);
+			}
+			
 			if ($_POST["keyword"] == '')
 				return;
 			print_r("<h3>[KEYWORD]: " .  $_POST["keyword"] . "</h3><br/>");
-			$content = shell_exec('find ./Books/ -type f -iname "*' . $_POST["keyword"] . '*.pdf"');
-			$data = explode("\n", $content);
+			
 			$prefix = "http://imaginelab.cn/pdfviewer/web/viewer.html?file=";
+			$validType = array('pdf'=>1, 'mobi'=>2, 'epub'=>3, 'txt'=>4);
+			$content = shell_exec('find ./Books/ -type f -iname "*' . $_POST["keyword"] . '*"');
+			$data = explode("\n", $content);
+			
 			foreach ($data as $path) {
-				$link = $prefix . $path;
-				$name = explode('/', "$path");
-				$name = end($name);
-				if ($name=='')
+				if ($path=='')
 					break;
-				echo "<input type=\"checkbox\" name=\"checkbox[]\" value=$path> <img src=\"./icon/pdf.png\"/> ";
-				echo "<a href='{$link}' title=''> $name </a> <br/>";
+				$link = $prefix . $path;
+				$name = getBasename($path);
+				$type = getFileType($name);
+				if ($validType[$type] > 0) {
+					echo "<input type=\"checkbox\" name=\"selected[]\" value=\"$path\"> <img src=\"./icon/{$type}.png\"/> ";
+					if ($type == "pdf")
+						echo "<a href='{$link}' title=''> $name </a> <br/>";
+					else 
+						echo $name . "<br/>";
+				}
 			}
 		?>
         <br/><br/>
